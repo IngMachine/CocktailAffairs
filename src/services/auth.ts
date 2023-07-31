c4d11553b7a519ee1ec52b3b0231808708791a35
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-31 18:39:44 -0500

    Services de auth, nos conectamos con mongo por medio del modelo e implementamos la logica de cada uno de los servicios a utilizar

---
+++ src/services/auth.ts
@@ -0,0 +1,42 @@
+import UserModel from "../models/user";
+import {User} from "../interfaces/user.interface";
+import {encrypt, verified} from "../utils/bcrypt.handle";
+import {Auth} from "../interfaces/auth.interface";
+import {generateToken} from "../utils/jwt.handle";
+
+const createUser = async ({ email, password, name }: User) => {
+    const checkIs = await UserModel.findOne( { email });
+    const passwordHash = await encrypt(password);
+    if( checkIs ) return "ALREADY_USER";
+
+    const user = await UserModel.create({email, password: passwordHash, name});
+    const token = await generateToken(user.id);
+
+    return {
+        token,
+        user
+    };
+}
+
+const loginUser = async( { email, password}: Auth) => {
+    const checkIs = await UserModel.findOne( { email });
+    if( !checkIs ) return "NOT_FOUND_USER";
+
+    const passwordHash = checkIs.password;
+    const isCorrect = await verified(password, passwordHash);
+
+    if( !isCorrect ) return "PASSWORD_INCORRECT";
+
+    const token = await generateToken(checkIs.id)
+
+    return {
+        token,
+        user: checkIs
+    };
+
+}
+
+export {
+    createUser,
+    loginUser
+}
