5dc61ec7ef80ab7b0b01907b0560569db42cc818
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-31 17:27:49 -0500

    Definicion de las rutas de auth

---
+++ src/routes/auth.ts
@@ -0,0 +1,16 @@
+import { Router} from "express";
+import {loginController, registerController} from "../controllers/auth";
+
+const router =  Router();
+
+/**
+ * http://localhost:3002/auth/register [POST]
+ */
+router.post('/register', registerController);
+
+/**
+ * http://localhost:3002/auth/login [POST]
+ */
+router.post('/login', loginController)
+
+export { router }
