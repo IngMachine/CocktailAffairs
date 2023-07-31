3ee9f6430ea0f66d3c6b0ed25f5b74463efec27f
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-31 05:17:03 -0500

    Implementación basica de montar el servidor express en desarrollo

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
+import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
