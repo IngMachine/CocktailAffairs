1561c1a642ee53586ceb7a0432b333531e69796a
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-02 16:37:55 -0500

    Modelo de imageCocktail para guardar en mongoose

---
+++ src/models/imageCocktail.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+import {ImageCocktail} from "../interfaces/imageCocktail.interface";
+
+const ImageCocktailSchema = new Schema<ImageCocktail>(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+ImageCocktailSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+})
+
+const ImageCocktailModel = model('imageCocktail', ImageCocktailSchema);
+
+export default ImageCocktailModel;
