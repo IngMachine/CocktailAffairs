b23d20d2558bb398e7f5cf919d373fd890c0e558
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-03 18:03:05 -0500

    Interface de role

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,8 @@
 import {Auth} from "./auth.interface";
+import {Role} from "./role.interface";
 
 export interface User extends Auth {
      name: string;
      description: string;
+     role: Role[]
 }
