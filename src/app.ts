810a4331cf3f294d20b778334171963931a38981
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-02 20:12:35 -0500

    configuracion de fileUpload para poner un archivo temporal

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
-import 'dotenv/config'
 import express from 'express';
+import fileUpload from 'express-fileupload'
 import cors from 'cors';
+
+import 'dotenv/config'
 import db from './config/mongo'
+
 import {router} from "./routes";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './src/uploads'
+}));
 app.use(router)
 db().then( () => console.log('Connection established'));
 
