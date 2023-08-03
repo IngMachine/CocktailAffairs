55996ff31593f7a9d2cf546b151244d5b07826df
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-03 17:46:01 -0500

    Quitar console log del error

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,7 +1,6 @@
 import {Response} from "express";
 
 const handleHttp = ( res: Response, error: string, errorRaw ?: any ) => {
-    console.error(errorRaw);
     res.status(500).json({
         error
     })
