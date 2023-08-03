f713317ead52e2da42c54c77161d72ff0a6a42ed
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-03 17:54:27 -0500

    Validacion de los campos cuando se esta creando un usuario

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,23 @@
 import { Router} from "express";
 import {loginController, registerController} from "../controllers/auth";
+import {check} from "express-validator";
+import {fieldsValidators} from "../middleware/fields-validators";
 
 const router =  Router();
 
 /**
  * http://localhost:3002/auth/register [POST]
  */
-router.post('/register', registerController);
+router.post(
+    '/register',
+    [
+        check('name', 'The name is required').not().notEmpty(),
+        check('password', 'The password is required').not().notEmpty().isLength({min: 6}).withMessage('The password is minimum the 6 characters'),
+        check('email', 'The email is required').not().notEmpty().isEmail().withMessage('The email not valid format'),
+        fieldsValidators
+    ],
+    registerController
+);
 
 /**
  * http://localhost:3002/auth/login [POST]
