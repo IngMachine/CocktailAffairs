788487a451da78a6b24ba4d6ad78cc8cbe51114d
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-03 18:17:46 -0500

    TODO de sistema de log

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,5 +1,6 @@
 import {Response} from "express";
 
+// TODO: Utilizar un sistema de log para cuando pase algo
 const handleHttp = ( res: Response, error: string, errorRaw ?: any ) => {
     res.status(500).json({
         error
