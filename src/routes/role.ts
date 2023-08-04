2771a599c471447384a8e244baa2c5c1244abedd
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-04 07:49:47 -0500

    Implementacion del middleware de checkRolPermit en role

--- src/routes/role.ts
+++ src/routes/role.ts
@@ -6,11 +6,14 @@ import {
     updateRoleController,
     deleteRoleController
 } from "../controllers/role";
-import {checkJWT} from "../middleware/session";
+
+import {checkJWT, checkRolPermit} from "../middleware/session";
+
+import {RoleEnum} from "../constant/role";
 
 const router =  Router();
 
-router.use(checkJWT);
+router.use([checkJWT, checkRolPermit([RoleEnum.Admin])]);
 
 /**
  * http://localhost:3002/role/ [GET]
