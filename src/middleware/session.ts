61c00df40c64467d44f2e8d8035143b841a1bd31
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-04 07:49:20 -0500

    middleware para saber si el rol es permitido en la api

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Response} from "express";
 import {verifyToken} from "../utils/jwt.handle";
 import {RequestExt} from "../interfaces/req-ext.interface";
+import {getUserService} from "../services/user";
 
 const checkJWT = (req: RequestExt, res: Response, next: NextFunction) => {
     try {
@@ -25,6 +26,44 @@ const checkJWT = (req: RequestExt, res: Response, next: NextFunction) => {
     }
 }
 
+const checkRolPermit = (roles: string[]) => async(req: RequestExt, res: Response, next: NextFunction) => {
+    try {
+        const jwtByUser = req.headers.authorization || null;
+        const jwt = jwtByUser?.split(' ').pop();
+        const {id} = verifyToken(`${jwt}`) as { id: string };
+
+        const userService = await getUserService(id);
+
+        const userServiceObj = userService!.toObject();
+
+        const rolesUser = userServiceObj.role.map( rol => {
+            return {
+                ...rol,
+                // @ts-ignore
+                _id: rol._id.toString()
+            }
+        })
+
+        const isIncluded = rolesUser.some(role => roles.map(rol => rol.toUpperCase()).includes(role.name));
+
+        if(isIncluded) {
+            next();
+        } else {
+            res.status(409).json({
+                ok: false,
+                msg: 'No permissions'
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Talk with administration - Auth permit'
+        })
+    }
+}
+
+
 export {
-    checkJWT
+    checkJWT,
+    checkRolPermit
 }
