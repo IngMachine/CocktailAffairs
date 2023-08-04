bf09449edafb04737f8db5d71e733fefdced301d
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-03 20:41:36 -0500

    Controlador de usuario, obtener todos los usuarios, editar un usuario e eliminar un usuario

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,16 +1,43 @@
 import {Request, Response} from "express";
 
+import {
+    getUsersService,
+    updateUserService,
+    deleteUserService
+} from "../services/user";
+
+import {handleHttp} from "../utils/error.handle";
+import {deleteCocktail} from "../services/cocktails";
+
 
 const getUsersController = async(req: Request, res: Response) => {
-
+    try {
+        const responseUsers = await getUsersService();
+        return res.status(200).json(responseUsers);
+    } catch (err) {
+        console.log(err)
+        handleHttp(res, 'ERROR_GET_USERS', err);
+    }
 }
 
-const updateUserController = async(req: Request, res: Response) => {
-
+const updateUserController = async({params, body}: Request, res: Response) => {
+    try {
+        const {id} = params;
+        const responseUser = await updateUserService(id, body)
+        return res.status(200).json(responseUser)
+    } catch (err) {
+        handleHttp(res, 'ERROR_UPDATE_USER', err)
+    }
 }
 
-const deleteUserController = async(req: Request, res: Response) => {
-
+const deleteUserController = async({params}: Request, res: Response) => {
+    try {
+        const {id} = params;
+        const responseCocktail = await deleteUserService(id);
+        res.json(responseCocktail);
+    } catch (err) {
+        handleHttp(res, 'ERROR_DELETE_USER', err);
+    }
 }
 
 export {
