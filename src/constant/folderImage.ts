46734ad7a2b55c2fdab1daeddf53ae89e7e5a2ba
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-04 18:52:37 -0500

    Link de la imagen cuando no hay photo por subir

--- src/constant/folderImage.ts
+++ src/constant/folderImage.ts
@@ -2,3 +2,8 @@ export enum FolderImage {
     Cocktails = 'cocktails',
     Bartenders = 'bartenders'
 }
+
+
+export enum LinkImage {
+    NoPhoto = "https://res.cloudinary.com/journal-app-react-2023/image/upload/v1691189634/nophoto.jpg"
+}
