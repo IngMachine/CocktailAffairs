b2eda3fba1180b36a970bd2ff6881a1c5604778d
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-05 16:27:11 -0500

    controlador para crear un booking

--- src/controllers/booking.ts
+++ src/controllers/booking.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 
 import {
     getBookingsService,
+    createBookingService
 } from "../services/booking";
 
 import {handleHttp} from "../utils/error.handle";
@@ -16,6 +17,17 @@ const getBookingsController = async (req: Request, res: Response) => {
     }
 }
 
+const createBookingController = async ({body}: Request, res: Response) => {
+    try {
+        const responseBooking = await createBookingService(body);
+        res.status(200).json(responseBooking);
+    } catch (err) {
+        console.log(err)
+        handleHttp(res, 'ERROR_CREATE_BOOKING', err);
+    }
+}
+
 export {
-    getBookingsController
+    getBookingsController,
+    createBookingController
 }
