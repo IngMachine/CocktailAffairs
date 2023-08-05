0140d85c56c04b5622317a61b895e8a8798809ef
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-05 12:10:31 -0500

    Correcion de agregar el jwt a la ruta

--- src/routes/booking.ts
+++ src/routes/booking.ts
@@ -6,17 +6,20 @@ import {fieldsValidators} from "../middleware/fields-validators";
 import {
     getBookingController,
 } from "../controllers/booking";
-import {checkRolPermit} from "../middleware/session";
+import {checkJWT, checkRolPermit} from "../middleware/session";
 import {RoleEnum} from "../constant/role";
 
 const router =  Router();
 
-router.use( checkRolPermit([RoleEnum.Admin]))
+router.use([
+    checkJWT,
+    checkRolPermit( [ RoleEnum.Admin ] )
+])
 
 /**
  * http://localhost:3002/booking/ [Get]
  */
-router.post(
+router.get(
     '/',
     getBookingController
 );
