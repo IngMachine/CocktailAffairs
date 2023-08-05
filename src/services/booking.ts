f66b6a930290001a6b366ca134525120a697b068
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-05 16:27:22 -0500

    service para crear un booking

--- src/services/booking.ts
+++ src/services/booking.ts
@@ -1,14 +1,25 @@
-import Booking from "../models/booking";
+import BookingModel from "../models/booking";
+
+import {Booking} from "../interfaces/booking.interface";
 
 
 const getBookingsService = async() => {
     try {
-        return await Booking.find({});
+        return await BookingModel.find({});
+    } catch (err) {
+        throw err;
+    }
+}
+
+const createBookingService = async(booking: Booking) => {
+    try {
+        return await BookingModel.create(booking);
     } catch (err) {
         throw err;
     }
 }
 
 export {
-    getBookingsService
+    getBookingsService,
+    createBookingService
 }
