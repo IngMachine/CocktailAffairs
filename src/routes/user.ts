02b14cb825eb7238f5419e55d77d023e479051be
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-15 10:09:17 -0500

    Validaciones en cada una de las api. tanto params como campos necesarios

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,5 @@
 import { Router} from "express";
+import {check, param} from "express-validator";
 
 import {
     getUsersController,
@@ -7,18 +8,17 @@ import {
     deleteUserController
 } from "../controllers/user";
 
-import {checkJWT} from "../middleware/session";
+import {checkJWT, checkRolPermit} from "../middleware/session";
+import {fieldsValidators} from "../middleware/fields-validators";
+import {RoleEnum} from "../constant/role";
 
 const router =  Router();
 
-router.use(checkJWT);
-
-/**
- * http://localhost:3002/user/ [GET]
- */
-router.get(
-    '/',
-    getUsersController
+router.use(
+    [
+        checkJWT,
+        checkRolPermit([RoleEnum.Admin, RoleEnum.Visitor])
+    ]
 );
 
 /**
@@ -34,7 +34,26 @@ router.get(
  */
 router.put(
     '/:id',
+    [
+        param(':id', 'The id is required')
+            .not()
+            .notEmpty()
+            .isMongoId().withMessage('The id is invalid'),
+        check('name', 'Name is required').not().notEmpty()
+            .isLength({min: 3}).withMessage('Name is too short'),
+        check('description', 'Description is too short').isLength({ min: 6}),
+        fieldsValidators
+    ],
     updateUserController
+);
+
+router.use( checkRolPermit([RoleEnum.Admin]))
+/**
+ * http://localhost:3002/user/ [GET]
+ */
+router.get(
+    '/',
+    getUsersController
 );
 
 /**
@@ -42,6 +61,13 @@ router.put(
  */
 router.delete(
     '/:id',
+    [
+        param(':id', 'The id is required')
+            .not()
+            .notEmpty()
+            .isMongoId().withMessage('The id is invalid'),
+        fieldsValidators
+    ],
     deleteUserController
 )
 
