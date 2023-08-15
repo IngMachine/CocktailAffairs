3f2a36b94ff0e534c8d16c87ef17c63f19f6282f
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-15 10:58:36 -0500

    Remover todo del servicio de crear una imagen de coctel

--- src/services/cocktails.ts
+++ src/services/cocktails.ts
@@ -1,140 +1,58 @@
-import fs from 'fs-extra'
-
-import {deleteImageCloudinary, uploadImageCloudinary} from "../config/cloudinary";
-
-import {Cocktail} from "../interfaces/cocktails.interface";
-import {ImageCocktail} from "../interfaces/imageCocktail.interface";
-
 import CocktailModel from "../models/cocktail";
-import ImageCocktailModel from "../models/imageCocktail";
-import {FolderImage} from "../constant/folderImage";
+import {Cocktail} from "../interfaces/cocktails.interface";
 
-const getCocktails = async() => {
-    return await CocktailModel
-                        .find()
-                        .populate('user', '-_id -password -createdAt -updatedAt')
-                        .populate('imageCocktail', '-_id name url');
+const getCocktailsService = async() => {
+    try {
+        return await CocktailModel
+            .find()
+            .populate('user', '-_id -password -createdAt -updatedAt')
+            .populate('imageCocktail', '-_id name url');
+    } catch (err) {
+        throw err;
+    }
+
 }
 
-const getCocktail = async(id: string) => {
-    return await CocktailModel
-                            .findById(id)
-                            .populate('user', '-_id -password -createdAt -updatedAt')
-                            .populate('imageCocktail', '-_id name url');
+const getCocktailService = async(id: string) => {
+    try {
+        return await CocktailModel
+            .findById(id)
+            .populate('user', '-_id -password -createdAt -updatedAt')
+            .populate('imageCocktail', '-_id name url');
+    } catch (err) {
+        throw err;
+    }
 }
-const insertCocktail = async ( cocktail: Cocktail) => {
+const createCocktailService = async ( cocktail: Cocktail) => {
     return await CocktailModel.create(cocktail);
 }
 
-const updateCocktail = async (id: string, cocktail: Cocktail) => {
-    return await CocktailModel.findByIdAndUpdate(
-        id,
-        cocktail,
-        {
-            new: true
-        }
-    );
-}
-
-const deleteCocktail = async (id: string) => {
-    return await CocktailModel.findByIdAndDelete(id);
-}
-
-const getImage = async () => {
-    return await ImageCocktailModel.find({});
-}
-
-const insertImage = async ( files: any, imageCocktail: ImageCocktail ) => {
+const updateCocktailService = async (id: string, cocktail: Cocktail) => {
     try {
-        let image = await ImageCocktailModel.findOne({ name: imageCocktail.name });
-        if ( image ) {
-            return {
-                ok: false,
-                msg: 'A image exists with this name'
+        return await CocktailModel.findByIdAndUpdate(
+            id,
+            cocktail,
+            {
+                new: true
             }
-        }
-        const result = await uploadImageCloudinary(
-            files.image.tempFilePath,
-            FolderImage.Cocktails,
-            imageCocktail.name
         );
-        await fs.unlink(files.image.tempFilePath)
-        return {
-            ok: true,
-            msg: await ImageCocktailModel.create({
-                ...imageCocktail,
-                ...result
-            })
-        };
     } catch (err) {
         throw err;
     }
 }
 
-const updateImageCocktail = async (files: any, imageCocktail: ImageCocktail, id: string ) => {
+const deleteCocktailService = async (id: string) => {
     try {
-        let image = await ImageCocktailModel.findById(id);
-        if (!image) {
-            return {
-                ok: false,
-                msg: 'A image no exists with this id'
-            }
-        }
-
-        await deleteImageCloudinary(image.public_id);
-
-        const result = await uploadImageCloudinary(
-            files.image.tempFilePath,
-            FolderImage.Cocktails,
-            imageCocktail.name
-        );
-
-        await fs.unlink(files.image.tempFilePath)
-        return {
-            ok: true,
-            msg: await ImageCocktailModel.findByIdAndUpdate(
-                id,
-                {
-                    ...imageCocktail,
-                    ...result
-                },
-                {
-                    new: true
-                }
-            )
-        };
-    } catch (err) {
-        throw err;
-    }
-}
-
-const deleteImageCocktail = async (id: string) => {
-    try {
-        let image = await ImageCocktailModel.findById(id);
-        if (!image) {
-            return {
-                ok: false,
-                msg: 'A image no exists with this id'
-            }
-        }
-        await deleteImageCloudinary(image.public_id);
-        return await ImageCocktailModel.findByIdAndDelete(id);
+        return await CocktailModel.findByIdAndDelete(id);
     } catch (err) {
         throw err;
     }
 }
 
 export {
-    // Cocktail
-    getCocktails,
-    getCocktail,
-    insertCocktail,
-    updateCocktail,
-    deleteCocktail,
-
-    // Image
-    getImage,
-    insertImage,
-    updateImageCocktail,
-    deleteImageCocktail
+    getCocktailsService,
+    getCocktailService,
+    createCocktailService,
+    updateCocktailService,
+    deleteCocktailService,
 }
