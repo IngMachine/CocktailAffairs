2c655ffd6f1b71896a91722cdc5efa80ce2f46bd
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-15 11:19:20 -0500

    Ampliar la informacion de obtener de los servicios de la base de datos

--- src/services/customer.ts
+++ src/services/customer.ts
@@ -17,7 +17,10 @@ const createCustomerService = async ( customer: Customer ) => {
         const user = await getUserService(customer.user as string);
         if( !customerDB && user ) {
             await updateRoleUserByIdService(customer.user as string, RoleEnum.Customer);
-            return await CustomerModel.create(customer);
+            return {
+                ok: true,
+                msg: await CustomerModel.create(customer)
+            }
         } else {
             return {
                 ok: false,
@@ -30,29 +33,45 @@ const createCustomerService = async ( customer: Customer ) => {
 }
 
 const getCustomerByIdUserService = async (idUser: string) => {
-    return await CustomerModel.findOne({ user: idUser })
-        .select('_id');
+    try {
+        return await CustomerModel.findOne({ user: idUser })
+            .select('_id phoneNumber shippingAddress')
+            .populate('user', 'name email description _id');
+    } catch (err) {
+        throw err;
+    }
 }
 
-const updateCustomerService = async (customer: Customer, id: string) => {
+const getCustomerByIdService = async (id: string) => {
+    try {
+        return await CustomerModel.findById(id);
+    } catch (err) {
+        throw err;
+    }
+}
+
+const updateCustomerService = async (id: string, customer: Customer ) => {
     try {
         const user = await getUserService(customer.user as string);
         const customerDB = await CustomerModel.findById(id);
 
-        if (user) {
+        if (customerDB?.user!.toString() === user?._id.toString() && customerDB) {
 
             const newCustomer: Customer = {
-                ...customerDB!.toObject(),
+                ...customerDB.toObject(),
                 ...customer
             };
 
-            return await CustomerModel.findByIdAndUpdate(
-                id,
-                newCustomer,
-                {
-                    new: true,
-                }
-            );
+            return {
+                ok: true,
+                msg: await CustomerModel.findByIdAndUpdate(
+                    id,
+                    newCustomer,
+                    {
+                        new: true,
+                    }
+                )
+            }
         } else {
             return {
                 ok: false,
@@ -65,9 +84,19 @@ const updateCustomerService = async (customer: Customer, id: string) => {
     }
 }
 
+const deleteCustomerByIdService = async (id: string) => {
+    try {
+        return CustomerModel.findByIdAndDelete(id);
+    } catch (err) {
+        throw err;
+    }
+}
+
 export {
     getCustomersService,
     createCustomerService,
     getCustomerByIdUserService,
-    updateCustomerService
+    getCustomerByIdService,
+    updateCustomerService,
+    deleteCustomerByIdService
 }
