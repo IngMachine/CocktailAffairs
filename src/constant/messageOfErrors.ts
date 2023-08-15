3b22d129d111b40570318ba28961e6eae5990ba2
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-15 09:48:28 -0500

    Constante de mensaje de errores

---
+++ src/constant/messageOfErrors.ts
@@ -0,0 +1,4 @@
+// TODO: poner todas las frases en esta constante para que tengamos un solo lugar del conocimiento o verdad.
+export enum MessageErrorsEnum {
+    EmailOrPasswordIncorrect = "The provided credentials are invalid. Please check your email and password and try again."
+}
