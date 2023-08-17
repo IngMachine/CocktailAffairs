49c09c7591f9928914f18421b2e6b34cb7573925
Fredy Rivera <fredyjesus2013@gmail.com>
2023-08-17 13:21:04 -0500

    Base de datos conexion

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import 'dotenv/config'
 import db from './config/mongo'
 
 import {router} from "./V1/routes";
+import {checkJWT, checkRolPermit} from "./middleware/session";
+import {RoleEnum} from "./constant/role";
 
 const PORT = process.env.PORT || '3000';
 
@@ -15,8 +17,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerV1Setup));
 db().then( () => console.log('Connection established'));
+
+// TODO: habilitar la pagina de la documentacion solo para usuario registrados y administrativos.
+// app.use([checkJWT, checkRolPermit([RoleEnum.Admin])])
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerV1Setup));
 
 
 app.listen(PORT, () => {
